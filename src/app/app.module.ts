22cb496081876878724af0e9289ee7e6a5fc5b72
unknown <sureshkumar.ece15@gmail.com>
2020-03-18 19:45:30 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { GridsterModule } from 'angular-gridster2';
+
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    GridsterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
